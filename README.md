c6536f4189a60a00b91d048f08e589b6866e92c0
zaikunzhang <zaikunzhang@gmail.com>
2021-09-18 21:23:24 +0800

    update comments

--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 `infnan.f90` implements a module providing some functions for checking Inf/NaN, which aim to work
 even when compilers are invoked with aggressive optimization flags, such as `gfortran -Ofast`.
 
-There are many ways to implement functions like `is_nan`. However, not all of them work with
+Many ways exist to implement functions like `is_nan`. However, not all of them work with
 aggressive optimization flags. For example, there are such discussions on
-[StackOverflow](https://stackoverflow.com/questions/15944614/is-it-possible-to-make-isnan-work-in-gfortran-o3-ffast-math/69217593?noredirect=1#comment122344217_69217593).
+[StackOverflow](https://stackoverflow.com/questions/15944614).
 
 The `ieee_is_nan` included in `ieee_arithmetic` of `gfortran 9.3.0` does not work with aggressive
 optimization flags like `-Ofast`.
 Given the fact that `-Ofast` implies `-ffinite-math-only`, we cannot blame `ieee_arithmetic` for
 the failure In addition, some compilers
 (`gfortran 9.3.0`, `ifort 21.0`, and `nagfor 7.0`) are buggy concerning the return
-type of `ieee_is_nan` when some special compilation options are imposed, as has been discussed
+kind of `ieee_is_nan` when some special compilation flags are imposed, as has been discussed
 on [Fortran Discourse](https://fortran-lang.discourse.group/t/is-this-expected-fortran-standard-is-not-respected-by-gfortran-fdefault-integer-8-ifort-i8-or-nagfor-i8).
 
 My choice of implementation is totally empirical, in the sense that I have not studied in-depth what the aggressive optimization flags really do, but only made some tests and found some implementation that worked correctly. In other words, I do not know why my implementation works but other implementations may not. The story may change when compilers are changed/updated.
--- infnan.f90
+++ infnan.f90
@@ -5,20 +5,22 @@ module infnan_mod
 ! 2. There are many ways to implement functions like `is_nan`. However, not all of them work with
 ! aggressive optimization flags. For example, for `gfortran 9.3.0`, the `ieee_is_nan` included in
 ! `ieee_arithmetic` does not work with `gfortran -Ofast`. See the following for discussions
-! https://stackoverflow.com/questions/15944614/is-it-possible-to-make-isnan-work-in-gfortran-o3-ffast-math
+! https://stackoverflow.com/questions/15944614
 !
 ! 3. My choice of implementation is totally empirical, in the sense that I have not studied in-depth
-! what the aggressive optimization flags really do, but only made some tests and found thee
+! what the aggressive optimization flags really do, but only made some tests and found the
 ! implementations that worked correctly. In other words, I do not know why my implementation works
 ! but other implementations may not. The story may change when compilers are changed/updated.
 !
-! 4. Even though the functions involve invocation of ABS and HUGE, their performance (in terms of
-! CPU time) turns out comparable to or even better than the functions in `ieee_arithmetic`.
+! 4. Do NOT change the functions without thorough testing. Their implementation is delicate. For
+! example, when compilers are invoked with aggressive optimization flags,
+! (X <= HUGE(X) .AND. X >= -HUGE(X)) may differ from (ABS(X) <= HUGE(X)) ,
+! (X > HUGE(X) .OR. X < -HUGE(X)) may differ from (ABS(X) > HUGE(X)) ,
+! (ABS(X) > HUGE(X) .AND. X > 0) may differ from (X > HUGE(X)) .
 !
-! 5. N.B.: Do NOT change the implementations without thorough testing. The implementations are
-! delicate. For example, when compilers are invoked with aggressive optimization flags,
-! (X <= HUGE(X) .AND. X >= -HUGE(X)) differs from (ABS(X) <= HUGE(X)) ,
-! (X > HUGE(X) .OR. X < -HUGE(X)) differs from (ABS(X) > HUGE(X)) .
+! 5. Even though the functions involve invocation of ABS and HUGE, their performance (in terms of
+! CPU time) turns out comparable to or even better than the functions in `ieee_arithmetic`.
+
 
 use consts_mod, only : SP, DP
 implicit none
